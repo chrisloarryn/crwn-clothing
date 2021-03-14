1e3dff43aff8538706826f00bc2315c9307a79ad
chrisloarryn <chrisloarryn@gmail.com>
2021-03-14 11:53:42 -0300

    Update collection-item.component.tsx

--- src/components/collection-item/collection-item.component.tsx
+++ src/components/collection-item/collection-item.component.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { ShopDataItemI } from 'mock/shop.data'
 
 import './collection-item.styles.scss'
 
-const CollectionItem: React.FC = () => {
+type CollectionItemProps = Pick<ShopDataItemI, 'id' | 'imageUrl' | 'name' | 'price'>
+
+const CollectionItem: React.FC<CollectionItemProps> = ({
+  id,
+  imageUrl,
+  name,
+  price,
+}) => {
+  console.log(id)
   return (
-    <div>
-      <h1>CollectionItem</h1>
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
     </div>
   )
 }
