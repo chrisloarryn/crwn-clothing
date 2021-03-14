c47a8097be4cba99934c4a2a7451fa22f7292703
chrisloarryn <chrisloarryn@gmail.com>
2021-03-14 11:44:40 -0300

    feat: rename folder and component and add styled / add collection-item component

---
+++ src/components/collection-item/collection-item.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './collection-item.styles.scss'
+
+const CollectionItem: React.FC = () => {
+  return (
+    <div>
+      <h1>CollectionItem</h1>
+    </div>
+  )
+}
+
+export default CollectionItem
